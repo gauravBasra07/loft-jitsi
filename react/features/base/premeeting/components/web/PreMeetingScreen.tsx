520ab5c6d5fbcd530692632ecfd5ae8eac008d46
gauravBasra <gaurav.basra@shinedezign.com>
2023-11-09 14:58:02 +0530

    lobby design changes

--- react/features/base/premeeting/components/web/PreMeetingScreen.tsx
+++ react/features/base/premeeting/components/web/PreMeetingScreen.tsx
@@ -190,13 +190,13 @@ const PreMeetingScreen = ({
                         <h1 className = { classes.title }>
                             {title}
                         </h1>
-                        {_roomName && (
+                        {/* {_roomName && (
                             <span className = { classes.roomName }>
                                 {_roomName}
                             </span>
-                        )}
+                        )} */}
                         {children}
-                        {_buttons.length && <Toolbox toolbarButtons = { _buttons } />}
+                        {/* {_buttons.length && <Toolbox toolbarButtons = { _buttons } />} */}
                         {skipPrejoinButton}
                         {showUnsafeRoomWarning && <UnsafeRoomWarning />}
                         {showDeviceStatus && <DeviceStatus />}
