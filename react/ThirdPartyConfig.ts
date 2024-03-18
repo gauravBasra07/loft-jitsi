413ef2e32ca120563a408435bfaa4045e040bc1e
gauravBasra07 <gaurav.basra@shinedezign.com>
2024-03-18 18:27:22 +0530

    new changes added

--- react/ThirdPartyConfig.ts
+++
@@ -1,8 +0,0 @@
-export const Config = {
-    BACKEND_URL: "https://frontm.ai/api",
-    API_KEY: "K3a5bY5AwJ6PkiJVqCVAPadnpySgxSR68IPP3SYC",
-    SELF_HOSTED_JITSI_SERVER: "loft.onship.app",
-    JITSI_APP_ID: "and0MDA5OA==",
-    JITSI_SECRET_KEY: "OFptTnh0ekVKOXZsM0c2",
-    ENTRY_TOKEN_SECRET_KEY: "xpdw7pvCLY"
-};
--- react/features/authentication/middleware.ts
+++ react/features/authentication/middleware.ts
@@ -1,21 +1,24 @@
-import { IStore } from '../app/types';
-import { APP_WILL_NAVIGATE } from '../base/app/actionTypes';
+import { IStore } from "../app/types";
+import { APP_WILL_NAVIGATE } from "../base/app/actionTypes";
 import {
     CONFERENCE_FAILED,
     CONFERENCE_JOINED,
-    CONFERENCE_LEFT
-} from '../base/conference/actionTypes';
-import { isRoomValid } from '../base/conference/functions';
-import { CONNECTION_ESTABLISHED, CONNECTION_FAILED } from '../base/connection/actionTypes';
-import { hideDialog } from '../base/dialog/actions';
-import { isDialogOpen } from '../base/dialog/functions';
+    CONFERENCE_LEFT,
+} from "../base/conference/actionTypes";
+import { isRoomValid } from "../base/conference/functions";
+import {
+    CONNECTION_ESTABLISHED,
+    CONNECTION_FAILED,
+} from "../base/connection/actionTypes";
+import { hideDialog } from "../base/dialog/actions";
+import { isDialogOpen } from "../base/dialog/functions";
 import {
     JitsiConferenceErrors,
-    JitsiConnectionErrors
-} from '../base/lib-jitsi-meet';
-import MiddlewareRegistry from '../base/redux/MiddlewareRegistry';
-import { parseURIString } from '../base/util/uri';
-import { openLogoutDialog } from '../settings/actions';
+    JitsiConnectionErrors,
+} from "../base/lib-jitsi-meet";
+import MiddlewareRegistry from "../base/redux/MiddlewareRegistry";
+import { parseURIString } from "../base/util/uri";
+import { openLogoutDialog } from "../settings/actions";
 
 import {
     CANCEL_LOGIN,
@@ -23,8 +26,8 @@ import {
     LOGOUT,
     STOP_WAIT_FOR_OWNER,
     UPGRADE_ROLE_FINISHED,
-    WAIT_FOR_OWNER
-} from './actionTypes';
+    WAIT_FOR_OWNER,
+} from "./actionTypes";
 import {
     hideLoginDialog,
     openLoginDialog,
@@ -33,12 +36,11 @@ import {
     redirectToDefaultLocation,
     setTokenAuthUrlSuccess,
     stopWaitForOwner,
-    waitForOwner
-} from './actions';
-import { LoginDialog, WaitForOwnerDialog } from './components';
-import { getTokenAuthUrl, isTokenAuthEnabled } from './functions';
-import logger from './logger';
-
+    waitForOwner,
+} from "./actions";
+import { LoginDialog, WaitForOwnerDialog } from "./components";
+import { getTokenAuthUrl, isTokenAuthEnabled } from "./functions";
+import logger from "./logger";
 
 /**
  * Middleware that captures connection or conference failed errors and controls
@@ -49,172 +51,201 @@ import logger from './logger';
  * @param {Store} store - Redux store.
  * @returns {Function}
  */
-MiddlewareRegistry.register(store => next => action => {
+MiddlewareRegistry.register((store) => (next) => (action) => {
     switch (action.type) {
-    case CANCEL_LOGIN: {
-        const { dispatch, getState } = store;
-        const state = getState();
-        const { thenableWithCancel } = state['features/authentication'];
+        case CANCEL_LOGIN: {
+            const { dispatch, getState } = store;
+            const state = getState();
+            const { thenableWithCancel } = state["features/authentication"];
 
-        thenableWithCancel?.cancel();
+            thenableWithCancel?.cancel();
 
-        // The LoginDialog can be opened on top of "wait for owner". The app
-        // should navigate only if LoginDialog was open without the
-        // WaitForOwnerDialog.
-        if (!isDialogOpen(store, WaitForOwnerDialog)) {
+            // The LoginDialog can be opened on top of "wait for owner". The app
+            // should navigate only if LoginDialog was open without the
+            // WaitForOwnerDialog.
+            if (!isDialogOpen(store, WaitForOwnerDialog)) {
+                if (_isWaitingForOwner(store)) {
+                    // Instead of hiding show the new one.
+                    const result = next(action);
+
+                    dispatch(openWaitForOwnerDialog());
+
+                    return result;
+                }
+
+                dispatch(hideLoginDialog());
+
+                const { authRequired, conference } =
+                    state["features/base/conference"];
+                const { passwordRequired } = state["features/base/connection"];
+
+                // Only end the meeting if we are not already inside and trying to upgrade.
+                // NOTE: Despite it's confusing name, `passwordRequired` implies an XMPP
+                // connection auth error.
+                if ((passwordRequired || authRequired) && !conference) {
+                    dispatch(redirectToDefaultLocation());
+                }
+            }
+            break;
+        }
+
+        case CONFERENCE_FAILED: {
+            const { error } = action;
+
+            // XXX The feature authentication affords recovery from
+            // CONFERENCE_FAILED caused by
+            // JitsiConferenceErrors.AUTHENTICATION_REQUIRED.
+            let recoverable;
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const [_lobbyJid, lobbyWaitingForHost] = error.params;
+
+            if (
+                error.name === JitsiConferenceErrors.AUTHENTICATION_REQUIRED ||
+                (error.name === JitsiConferenceErrors.MEMBERS_ONLY_ERROR &&
+                    lobbyWaitingForHost)
+            ) {
+                if (typeof error.recoverable === "undefined") {
+                    error.recoverable = true;
+                }
+                recoverable = error.recoverable;
+            }
+            if (recoverable) {
+                store.dispatch(waitForOwner());
+            } else {
+                store.dispatch(stopWaitForOwner());
+            }
+            break;
+        }
+
+        case CONFERENCE_JOINED: {
+            const { dispatch, getState } = store;
+            const state = getState();
+            const config = state["features/base/config"];
+
+            let url: any = state["features/base/connection"]?.locationURL?.href;
+            sessionStorage.setItem("locationURL", url);
+
+            let origin: any =
+                state["features/base/connection"]?.locationURL?.origin;
+            sessionStorage.setItem("origin", origin);
+
+            let meetingId: any = state["features/base/conference"]?.room;
+            sessionStorage.setItem("meetingId", meetingId);
+
+            let userData: any = state["features/base/jwt"].user;
+
+            sessionStorage.setItem("userEmail", userData.email);
+            sessionStorage.setItem("name", userData.name);
+
+            if (
+                isTokenAuthEnabled(config) &&
+                config.tokenAuthUrlAutoRedirect &&
+                state["features/base/jwt"].jwt
+            ) {
+                // auto redirect is turned on and we have succesfully logged in
+                // let's mark that
+                dispatch(setTokenAuthUrlSuccess(true));
+            }
+
             if (_isWaitingForOwner(store)) {
-                // Instead of hiding show the new one.
-                const result = next(action);
-
-                dispatch(openWaitForOwnerDialog());
-
-                return result;
+                store.dispatch(stopWaitForOwner());
             }
-
-            dispatch(hideLoginDialog());
-
-            const { authRequired, conference } = state['features/base/conference'];
-            const { passwordRequired } = state['features/base/connection'];
-
-            // Only end the meeting if we are not already inside and trying to upgrade.
-            // NOTE: Despite it's confusing name, `passwordRequired` implies an XMPP
-            // connection auth error.
-            if ((passwordRequired || authRequired) && !conference) {
-                dispatch(redirectToDefaultLocation());
-            }
-        }
-        break;
-    }
-
-    case CONFERENCE_FAILED: {
-        const { error } = action;
-
-        // XXX The feature authentication affords recovery from
-        // CONFERENCE_FAILED caused by
-        // JitsiConferenceErrors.AUTHENTICATION_REQUIRED.
-        let recoverable;
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const [ _lobbyJid, lobbyWaitingForHost ] = error.params;
-
-        if (error.name === JitsiConferenceErrors.AUTHENTICATION_REQUIRED
-            || (error.name === JitsiConferenceErrors.MEMBERS_ONLY_ERROR && lobbyWaitingForHost)) {
-            if (typeof error.recoverable === 'undefined') {
-                error.recoverable = true;
-            }
-            recoverable = error.recoverable;
-        }
-        if (recoverable) {
-            store.dispatch(waitForOwner());
-        } else {
-            store.dispatch(stopWaitForOwner());
-        }
-        break;
-    }
-
-    case CONFERENCE_JOINED: {
-        const { dispatch, getState } = store;
-        const state = getState();
-        const config = state['features/base/config'];
-
-        if (isTokenAuthEnabled(config)
-            && config.tokenAuthUrlAutoRedirect
-            && state['features/base/jwt'].jwt) {
-            // auto redirect is turned on and we have succesfully logged in
-            // let's mark that
-            dispatch(setTokenAuthUrlSuccess(true));
-        }
-
-        if (_isWaitingForOwner(store)) {
-            store.dispatch(stopWaitForOwner());
-        }
-        store.dispatch(hideLoginDialog());
-        break;
-    }
-
-    case CONFERENCE_LEFT:
-        store.dispatch(stopWaitForOwner());
-        break;
-
-    case CONNECTION_ESTABLISHED:
-        store.dispatch(hideLoginDialog());
-        break;
-
-    case CONNECTION_FAILED: {
-        const { error } = action;
-        const state = store.getState();
-        const { jwt } = state['features/base/jwt'];
-
-        if (error
-                && error.name === JitsiConnectionErrors.PASSWORD_REQUIRED
-                && typeof error.recoverable === 'undefined'
-                && !jwt) {
-            error.recoverable = true;
-
-            _handleLogin(store);
-        }
-
-        break;
-    }
-
-    case LOGIN: {
-        _handleLogin(store);
-
-        break;
-    }
-
-    case LOGOUT: {
-        _handleLogout(store);
-
-        break;
-    }
-
-    case APP_WILL_NAVIGATE: {
-        const { dispatch, getState } = store;
-        const state = getState();
-        const config = state['features/base/config'];
-        const room = state['features/base/conference'].room;
-
-        if (isRoomValid(room)
-            && config.tokenAuthUrl && config.tokenAuthUrlAutoRedirect
-            && state['features/authentication'].tokenAuthUrlSuccessful
-            && !state['features/base/jwt'].jwt) {
-            // if we have auto redirect enabled, and we have previously logged in successfully
-            // we will redirect to the auth url to get the token and login again
-            // we want to mark token auth success to false as if login is unsuccessful
-            // the participant can join anonymously and not go in login loop
-            dispatch(setTokenAuthUrlSuccess(false));
-        }
-
-        break;
-    }
-
-    case STOP_WAIT_FOR_OWNER:
-        _clearExistingWaitForOwnerTimeout(store);
-        store.dispatch(hideDialog(WaitForOwnerDialog));
-        break;
-
-    case UPGRADE_ROLE_FINISHED: {
-        const { error, progress } = action;
-
-        if (!error && progress === 1) {
             store.dispatch(hideLoginDialog());
+            break;
         }
-        break;
-    }
 
-    case WAIT_FOR_OWNER: {
-        _clearExistingWaitForOwnerTimeout(store);
+        case CONFERENCE_LEFT:
+            store.dispatch(stopWaitForOwner());
+            break;
 
-        const { handler, timeoutMs }: { handler: () => void; timeoutMs: number; } = action;
+        case CONNECTION_ESTABLISHED:
+            store.dispatch(hideLoginDialog());
+            break;
 
-        action.waitForOwnerTimeoutID = setTimeout(handler, timeoutMs);
+        case CONNECTION_FAILED: {
+            const { error } = action;
+            const state = store.getState();
+            const { jwt } = state["features/base/jwt"];
 
-        // The WAIT_FOR_OWNER action is cyclic, and we don't want to hide the
-        // login dialog every few seconds.
-        isDialogOpen(store, LoginDialog)
-            || store.dispatch(openWaitForOwnerDialog());
-        break;
-    }
+            if (
+                error &&
+                error.name === JitsiConnectionErrors.PASSWORD_REQUIRED &&
+                typeof error.recoverable === "undefined" &&
+                !jwt
+            ) {
+                error.recoverable = true;
+
+                _handleLogin(store);
+            }
+
+            break;
+        }
+
+        case LOGIN: {
+            _handleLogin(store);
+
+            break;
+        }
+
+        case LOGOUT: {
+            _handleLogout(store);
+
+            break;
+        }
+
+        case APP_WILL_NAVIGATE: {
+            const { dispatch, getState } = store;
+            const state = getState();
+            const config = state["features/base/config"];
+            const room = state["features/base/conference"].room;
+
+            if (
+                isRoomValid(room) &&
+                config.tokenAuthUrl &&
+                config.tokenAuthUrlAutoRedirect &&
+                state["features/authentication"].tokenAuthUrlSuccessful &&
+                !state["features/base/jwt"].jwt
+            ) {
+                // if we have auto redirect enabled, and we have previously logged in successfully
+                // we will redirect to the auth url to get the token and login again
+                // we want to mark token auth success to false as if login is unsuccessful
+                // the participant can join anonymously and not go in login loop
+                dispatch(setTokenAuthUrlSuccess(false));
+            }
+
+            break;
+        }
+
+        case STOP_WAIT_FOR_OWNER:
+            _clearExistingWaitForOwnerTimeout(store);
+            store.dispatch(hideDialog(WaitForOwnerDialog));
+            break;
+
+        case UPGRADE_ROLE_FINISHED: {
+            const { error, progress } = action;
+
+            if (!error && progress === 1) {
+                store.dispatch(hideLoginDialog());
+            }
+            break;
+        }
+
+        case WAIT_FOR_OWNER: {
+            _clearExistingWaitForOwnerTimeout(store);
+
+            const {
+                handler,
+                timeoutMs,
+            }: { handler: () => void; timeoutMs: number } = action;
+
+            action.waitForOwnerTimeoutID = setTimeout(handler, timeoutMs);
+
+            // The WAIT_FOR_OWNER action is cyclic, and we don't want to hide the
+            // login dialog every few seconds.
+            isDialogOpen(store, LoginDialog) ||
+                store.dispatch(openWaitForOwnerDialog());
+            break;
+        }
     }
 
     return next(action);
@@ -228,11 +259,10 @@ MiddlewareRegistry.register(store => next => action => {
  * @returns {void}
  */
 function _clearExistingWaitForOwnerTimeout({ getState }: IStore) {
-    const { waitForOwnerTimeoutID } = getState()['features/authentication'];
+    const { waitForOwnerTimeoutID } = getState()["features/authentication"];
 
     waitForOwnerTimeoutID && clearTimeout(waitForOwnerTimeoutID);
 }
-
 
 /**
  * Checks if the cyclic "wait for conference owner" task is currently scheduled.
@@ -241,7 +271,7 @@ function _clearExistingWaitForOwnerTimeout({ getState }: IStore) {
  * @returns {boolean}
  */
 function _isWaitingForOwner({ getState }: IStore) {
-    return Boolean(getState()['features/authentication'].waitForOwnerTimeoutID);
+    return Boolean(getState()["features/authentication"].waitForOwnerTimeoutID);
 }
 
 /**
@@ -253,13 +283,14 @@ function _isWaitingForOwner({ getState }: IStore) {
  */
 function _handleLogin({ dispatch, getState }: IStore) {
     const state = getState();
-    const config = state['features/base/config'];
-    const room = state['features/base/conference'].room;
-    const { locationURL = { href: '' } as URL } = state['features/base/connection'];
+    const config = state["features/base/config"];
+    const room = state["features/base/conference"].room;
+    const { locationURL = { href: "" } as URL } =
+        state["features/base/connection"];
     const { tenant } = parseURIString(locationURL.href) || {};
 
     if (!room) {
-        logger.warn('Cannot handle login, room is undefined!');
+        logger.warn("Cannot handle login, room is undefined!");
 
         return;
     }
@@ -270,16 +301,19 @@ function _handleLogin({ dispatch, getState }: IStore) {
         return;
     }
 
-    getTokenAuthUrl(config, room, tenant, true, locationURL)
-        .then((tokenAuthServiceUrl: string | undefined) => {
+    getTokenAuthUrl(config, room, tenant, true, locationURL).then(
+        (tokenAuthServiceUrl: string | undefined) => {
             if (!tokenAuthServiceUrl) {
-                logger.warn('Cannot handle login, token service URL is not set');
+                logger.warn(
+                    "Cannot handle login, token service URL is not set"
+                );
 
                 return;
             }
 
             return dispatch(openTokenAuthUrl(tokenAuthServiceUrl));
-        });
+        }
+    );
 }
 
 /**
@@ -292,7 +326,7 @@ function _handleLogin({ dispatch, getState }: IStore) {
  */
 function _handleLogout({ dispatch, getState }: IStore) {
     const state = getState();
-    const { conference } = state['features/base/conference'];
+    const { conference } = state["features/base/conference"];
 
     if (!conference) {
         return;
